f45ee7cea347758ee4c20014a639f9849c553cc7
Vallail Naqvi <vallail895@gmail.com>
2024-04-04 12:33:56 +0530

    updated self class

--- csvreader.rb
+++ csvreader.rb
@@ -1,64 +1,41 @@
 require 'csv'
 
 class CSVFileReader
-  @headers = []
-  @data = []
+  class << self
+    attr_accessor :data, :headers
 
-  def self.inherited(subclass)
-    file_name = subclass.name.downcase + 's.csv' # Converts the class name to the corresponding file name
-    csv_data = CSV.read(file_name, headers: true)
-    @headers = csv_data.headers
-    define_accessor_methods(subclass, @headers)
-  end
+    def inherited(subclass)
+      file_name = "#{subclass.name.downcase}s.csv"
+      csv_data = CSV.read(file_name, headers: true)
+      subclass.headers = csv_data.headers
+      define_accessor_methods(subclass, subclass.headers)
+    end
 
-  def self.define_accessor_methods(klass, headers)
-    headers.each do |header|
-      klass.instance_eval do
-        attr_accessor header.to_sym
+    def define_accessor_methods(klass, headers)
+      headers.each do |header|
+        klass.class_eval do
+          attr_accessor header.to_sym
+        end
       end
     end
+    def read_csv
+      #@data is storing only the headers data or the first row of data to avoid 
+      file_name = "#{name.downcase}s.csv"
+      @data ||= CSV.read(file_name, headers: true, header_converters: :symbol).map(&:to_hash)
+      # puts "Headers: #{@headers}"  # headers inspect
+      # puts "First row of data: #{@data}"  #Data inspect
+    end
+    def find_by(attribute, value)
+      read_csv if data.nil? || data.empty?
+      data.map { |row| new(row) }.find { |record| record.send(attribute).to_s == value.to_s }
+    end
   end
-
-  def self.read_csv
-    file_name = name.downcase + 's.csv'
-    @data = CSV.read(file_name, headers: true, header_converters: :symbol).map(&:to_hash)
-  end
-
-  def self.find_by(attribute, value)
-    read_csv if @data.nil? || @data.empty?
-    @data.map { |row| new(row) }.find { |record| record.send(attribute).to_s == value.to_s }
-  end
-
-  def initialize(attributes = {})
+    def initialize(attributes = {})
     attributes.each do |name, value|
+      # p name
+      # p value
       send("#{name}=", value)
     end
   end
 end
 
-# # Usage example for Student
-# class Student < CSVFileReader
-# end
-
-# s = Student.new
-# s.student_id = 10
-# s.student_name = "Amit"
-# s.teacher_name = "Shilesh"
-# s.subject = "Physics"
-
-# s1 = Student.find_by(:student_id, "3")
-# puts s1.student_name  # Output should be "kiran"
-# puts s1.teacher_name  # Output should be "manoj"
-
-# # Usage example for State
-# class State < CSVFileReader
-# end
-
-# st = State.new
-# st.name = "Bihar"
-# st.capital = "Patna"
-# st.population = "22Crore"
-
-# st1 = State.find_by(:name, "Karnataka")
-# puts st1.capital     # Output should be "bangalore"
-# puts st1.population  # Output should be "6crore"
--- test.rb
+++ test.rb
@@ -12,10 +12,6 @@ st.name = "Bihar"
 st.capital = "Patna"
 st.population = "22Crore"
 
-st1 = State.find_by("name", "Karnataka")
-puts st1.capital # this should print bangalore
-puts st1.population # this should print 6crore
-
 s = Student.new
 s.student_id = 10
 s.student_name = "Amit"
@@ -26,9 +22,9 @@ s1 = Student.find_by("student_id", 3)
 puts s1.student_name # this should print kiran
 puts s1.teacher_name # this should print manoj
 
-
-
-
+st1 = State.find_by("name", "Karnataka")
+puts st1.capital # this should print bangalore
+puts st1.population # this should print 6crore
 
 
 
