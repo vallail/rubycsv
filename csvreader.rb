346d434b93f9855d0f8457d4bbd90427386d0def
Vallail Naqvi <vallail895@gmail.com>
2024-04-05 10:00:37 +0530

    data and header under subclass

--- csvreader.rb
+++ csvreader.rb
@@ -1,41 +1,37 @@
 require 'csv'
 
 class CSVFileReader
-  class << self
-    attr_accessor :data, :headers
+  def self.inherited(subclass)
+    subclass.instance_eval do
+      @data = nil
+      @headers = nil
 
-    def inherited(subclass)
-      file_name = "#{subclass.name.downcase}s.csv"
-      csv_data = CSV.read(file_name, headers: true)
-      subclass.headers = csv_data.headers
-      define_accessor_methods(subclass, subclass.headers)
-    end
+      def read_csv
+        file_name = "#{self.name.downcase}s.csv"
+        @data ||= CSV.read(file_name, headers: true, header_converters: :symbol).map(&:to_hash)
+        @headers ||= @data.first.keys
+        define_accessor_methods(@headers)
+      end
 
-    def define_accessor_methods(klass, headers)
-      headers.each do |header|
-        klass.class_eval do
-          attr_accessor header.to_sym
+      def define_accessor_methods(headers)
+        headers.each do |header|
+          attr_accessor header
         end
       end
-    end
-    def read_csv
-      #@data is storing only the headers data or the first row of data to avoid 
-      file_name = "#{name.downcase}s.csv"
-      @data ||= CSV.read(file_name, headers: true, header_converters: :symbol).map(&:to_hash)
-      # puts "Headers: #{@headers}"  # headers inspect
-      # puts "First row of data: #{@data}"  #Data inspect
-    end
-    def find_by(attribute, value)
-      read_csv if data.nil? || data.empty?
-      data.map { |row| new(row) }.find { |record| record.send(attribute).to_s == value.to_s }
+
+      def find_by(attribute, value)
+        read_csv if @data.nil? || @data.empty?
+        record = @data.find { |row| row[attribute.to_sym].to_s == value.to_s }
+        record ? new(record) : nil
+      end
+
+      read_csv
     end
   end
-    def initialize(attributes = {})
+
+  def initialize(attributes = {})
     attributes.each do |name, value|
-      # p name
-      # p value
       send("#{name}=", value)
     end
   end
 end
-
